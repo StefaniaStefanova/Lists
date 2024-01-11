073fbb8b323b0ed3bdd6af29e02191253cee205a
StefaniaStefanova <StefaniaStefanova@users.noreply.github.com>
2024-01-11 18:39:15 +0200

    Add project files.

---
+++ 01. Change List/Program.cs
@@ -0,0 +1,24 @@
+﻿
+List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+string command = Console.ReadLine();
+while(command!="end")
+{
+    string[] commandpart = command.Split(" ");
+    string commandname = commandpart[0];
+    command = Console.ReadLine();
+    int element =int.Parse( commandpart[1]);
+    if (commandname == "Delete")
+    {
+        numbers.RemoveAll(number => number == element);
+
+    }
+    else if(commandname=="Insert")
+    {
+        int position = int.Parse(commandpart[2]);
+        numbers.Insert(position, element);
+    }
+}
+Console.WriteLine(string.Join(" ", numbers));
+
+
+
---
+++ 02. List of Products/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
